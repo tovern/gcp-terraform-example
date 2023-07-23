369982715b1ca0a1319655ef8079c44db4df4a78
Tom Vernon <tom@machineadvertising.com>
2023-07-23 09:53:13 +0100

    fix: reformat document

--- foundation/stage/projects/projects.tf
+++ foundation/stage/projects/projects.tf
@@ -1,7 +1,7 @@
 resource "google_project" "wimbledon" {
-  name       = "Wimbledon"
-  project_id = "ten-wimbledon-s-proj"
-  folder_id  = "811842790167"
+  name            = "Wimbledon"
+  project_id      = "ten-wimbledon-s-proj"
+  folder_id       = "811842790167"
   billing_account = "015FFD-A994CD-CCE392"
 
   labels = {
@@ -11,12 +11,12 @@ resource "google_project" "wimbledon" {
 }
 
 resource "google_project_service" "services_wimbledon" {
-  for_each = toset(var.wimbledon_services)
+  for_each                   = toset(var.wimbledon_services)
   project                    = google_project.wimbledon.id
   service                    = each.key
   disable_dependent_services = false
   disable_on_destroy         = false
-  depends_on = [google_project.wimbledon]
+  depends_on                 = [google_project.wimbledon]
 }
 
 resource "google_compute_shared_vpc_service_project" "wimbledon_vpc_service" {
@@ -25,9 +25,9 @@ resource "google_compute_shared_vpc_service_project" "wimbledon_vpc_service" {
 }
 
 resource "google_project" "rolandgarros" {
-  name       = "RolandGarros"
-  project_id = "ten-rolandgarros-s-proj"
-  folder_id  = "811842790167"
+  name            = "RolandGarros"
+  project_id      = "ten-rolandgarros-s-proj"
+  folder_id       = "811842790167"
   billing_account = "015FFD-A994CD-CCE392"
 
   labels = {
@@ -37,12 +37,12 @@ resource "google_project" "rolandgarros" {
 }
 
 resource "google_project_service" "services_rolandgarros" {
-  for_each = toset(var.rolandgarros_services)
+  for_each                   = toset(var.rolandgarros_services)
   project                    = google_project.rolandgarros.id
   service                    = each.key
   disable_dependent_services = false
   disable_on_destroy         = false
-  depends_on = [google_project.rolandgarros]
+  depends_on                 = [google_project.rolandgarros]
 }
 
 resource "google_compute_shared_vpc_service_project" "rolandgarros_vpc_service" {
